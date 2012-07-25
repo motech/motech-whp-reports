968c9b660f574d1e4ab3ce281d7b6a22953d6644
Balaji Narain <balajin@thoughtworks.com>
2012-07-25 16:28:50 +0530

    Dev, BalajiN | #0 | Adding profile to bootstrap database.
    	1. Profile drops the db
    	2. Creates db

--- whp-reports-migration/src/main/resources/db/setup/base_setup.sql
+++ whp-reports-migration/src/main/resources/db/setup/base_setup.sql
@@ -3,7 +3,9 @@
 --
 
 SET statement_timeout = 0;
+
 --SET client_encoding = 'SQL_ASCII';
+
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
@@ -16,9 +18,7 @@ DROP SCHEMA whp_reports cascade;
 
 CREATE SCHEMA whp_reports;
 
-
 ALTER SCHEMA whp_reports OWNER TO postgres;
-
 
 SET search_path = whp_reports, pg_catalog;
 
