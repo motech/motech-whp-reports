7652d8d84eee19bf06238b2f5ba590fe4119e038
Vishal Naik <vishalnaik@thoughtworks.com>
2012-08-07 12:34:00 +0530

    Vishal|Abhi #313 Made CallLogRequest Serializable

--- whp-reports-contract/src/main/java/org/motechproject/whp/reports/contract/CallLogRequest.java
+++ whp-reports-contract/src/main/java/org/motechproject/whp/reports/contract/CallLogRequest.java
@@ -2,13 +2,13 @@ package org.motechproject.whp.reports.contract;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Data
-public class CallLogRequest {
+public class CallLogRequest  implements Serializable {
     private String providerId;
     private Date startTime;
     private Date endTime;
     private String calledBy;
-
 }
