08c62cacd443cdb5691410c611f19c86e9458e47
Vishal Naik <vishalnaik@thoughtworks.com>
2012-08-09 15:38:49 +0530

    Vishal #0 Added default response status to controllers

--- whp-reports-web/src/main/java/org/motechproject/whp/reports/webservice/controller/AdherenceCaptureController.java
+++ whp-reports-web/src/main/java/org/motechproject/whp/reports/webservice/controller/AdherenceCaptureController.java
@@ -5,10 +5,12 @@ import org.motechproject.whp.reports.builder.DomainBuilder;
 import org.motechproject.whp.reports.contract.AdherenceCaptureRequest;
 import org.motechproject.whp.reports.service.PatientAdherenceSubmissionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequestMapping("/adherence")
@@ -22,6 +24,7 @@ public class AdherenceCaptureController extends BaseController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "measure")
+    @ResponseStatus(value = HttpStatus.OK)
     public void callLogs(@RequestBody AdherenceCaptureRequest adherenceRequest) {
         adherenceSubmissionService.save(DomainBuilder.buildAdherenceSubmission(adherenceRequest));
     }
--- whp-reports-web/src/main/java/org/motechproject/whp/reports/webservice/controller/CallLogCaptureController.java
+++ whp-reports-web/src/main/java/org/motechproject/whp/reports/webservice/controller/CallLogCaptureController.java
@@ -5,10 +5,12 @@ import org.motechproject.whp.reports.builder.DomainBuilder;
 import org.motechproject.whp.reports.contract.CallLogRequest;
 import org.motechproject.whp.reports.service.CallLogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequestMapping("/callLog")
@@ -22,6 +24,7 @@ public class CallLogCaptureController extends BaseController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "measure")
+    @ResponseStatus(value = HttpStatus.OK)
     public void callLogs(@RequestBody CallLogRequest callLogRequest) {
         callLogService.save(DomainBuilder.buildCallLog(callLogRequest));
     }
