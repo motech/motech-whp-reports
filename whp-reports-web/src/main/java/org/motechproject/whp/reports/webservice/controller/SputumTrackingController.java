340698d3cffa7db7ef5b892c971cccaa86d6557b
DheerendraNath <dheerenn@thoughtworks.com>
2012-10-29 17:15:44 +0530

    Dheeru, Abhi | #451 | Rectifying Spring-controller

--- whp-reports-web/src/main/java/org/motechproject/whp/reports/webservice/controller/SputumTrackingController.java
+++ whp-reports-web/src/main/java/org/motechproject/whp/reports/webservice/controller/SputumTrackingController.java
@@ -3,6 +3,7 @@ package org.motechproject.whp.reports.webservice.controller;
 import org.motechproject.whp.reports.builder.DomainBuilder;
 import org.motechproject.whp.reports.contract.SputumTrackingRequest;
 import org.motechproject.whp.reports.service.SputumTrackingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ public class SputumTrackingController {
 
     private SputumTrackingService sputumTrackingService;
 
+    @Autowired
     public SputumTrackingController(SputumTrackingService sputumTrackingService) {
         this.sputumTrackingService= sputumTrackingService;
     }
