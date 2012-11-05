5054674545d268f910488b02451a35932a42aed2
Vishal Naik <vishal@vishal.corporate.thoughtworks.com>
2012-11-05 19:29:27 +0530

    Vishal, Balaji #549 Making ContainerRegistrationCallLogRequest serializable

--- whp-reports-contract/src/main/java/org/motechproject/whp/reports/contract/ContainerRegistrationCallLogRequest.java
+++ whp-reports-contract/src/main/java/org/motechproject/whp/reports/contract/ContainerRegistrationCallLogRequest.java
@@ -2,10 +2,11 @@ package org.motechproject.whp.reports.contract;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Data
-public class ContainerRegistrationCallLogRequest {
+public class ContainerRegistrationCallLogRequest implements Serializable {
 
     private String callId;
     private String providerId;
