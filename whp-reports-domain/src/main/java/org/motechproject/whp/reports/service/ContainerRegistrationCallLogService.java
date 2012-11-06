1f6e37630948db5164e21a2e03679a2593cb90fc
Vishal Naik <vishal@vishal.corporate.thoughtworks.com>
2012-11-06 18:25:55 +0530

    Vishal #549 Making service transactional

--- whp-reports-domain/src/main/java/org/motechproject/whp/reports/service/ContainerRegistrationCallLogService.java
+++ whp-reports-domain/src/main/java/org/motechproject/whp/reports/service/ContainerRegistrationCallLogService.java
@@ -4,11 +4,16 @@ import org.motechproject.whp.reports.domain.measure.ContainerRegistrationCallLog
 import org.motechproject.whp.reports.repository.ContainerRegistrationCallLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class ContainerRegistrationCallLogService {
 
     private ContainerRegistrationCallLogRepository containerRegistrationCallLogRepository;
+
+    public ContainerRegistrationCallLogService() {
+    }
 
     @Autowired
     public ContainerRegistrationCallLogService(ContainerRegistrationCallLogRepository containerRegistrationCallLogRepository) {
