7284632674c8e149af43ee76ca077b4b927225b3
Vishal Naik <vishal@vishal.corporate.thoughtworks.com>
2012-11-06 19:25:56 +0530

    Vishal #0 Enabling REST exports on JPA repositories. Prevent exporting of update/delete APIs as REST APIs.

--- whp-reports-domain/src/main/java/org/motechproject/whp/reports/repository/AdherenceCallLogRepository.java
+++ whp-reports-domain/src/main/java/org/motechproject/whp/reports/repository/AdherenceCallLogRepository.java
@@ -2,7 +2,9 @@ package org.motechproject.whp.reports.repository;
 
 import org.motechproject.whp.reports.domain.measure.AdherenceCallLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.repository.annotation.RestResource;
 
-public interface AdherenceCallLogRepository extends JpaRepository<AdherenceCallLog, Long> {
+@RestResource(path = "_adherenceCallLogs")
+public interface AdherenceCallLogRepository extends MotechJpaRepository<AdherenceCallLog> {
 
 }
