66569c21cf07e120c2a576c1d25aeae36b56d545
Vishal Naik <vishal@vishal.corporate.thoughtworks.com>
2012-11-15 16:16:59 +0530

    adding reclinejs
    Vishal #0 Updated spring data rest backend

---
+++ whp-reports-web/src/main/webapp/js/app.js
@@ -0,0 +1,84 @@
+jQuery(function($) {
+    window.dataExplorer = null;
+    window.explorerDiv = $('.data-explorer-here');
+
+    var state = recline.View.parseQueryString(decodeURIComponent(window.location.search));
+    if (state) {
+        _.each(state, function(value, key) {
+            try {
+                value = JSON.parse(value);
+            } catch(e) {}
+            state[key] = value;
+        });
+    } else {
+        state.url = 'demo';
+    }
+    var dataset = null;
+    if (state.dataset || state.url) {
+        var datasetInfo = _.extend({
+                url: state.url,
+                backend: state.backend
+            },
+            state.dataset
+        );
+        dataset = new recline.Model.Dataset(datasetInfo);
+    } else {
+        alert("Set URL and backend as request params to load data.")
+    }
+
+
+    createExplorer(dataset, state);
+});
+
+
+// make Explorer creation / initialization in a function so we can call it
+// again and again
+var createExplorer = function(dataset, state) {
+    // remove existing data explorer view
+    var reload = false;
+    if (window.dataExplorer) {
+        window.dataExplorer.remove();
+        reload = true;
+    }
+    window.dataExplorer = null;
+    var $el = $('<div />');
+    $el.appendTo(window.explorerDiv);
+
+    var views = [
+        {
+            id: 'grid',
+            label: 'Grid',
+            view: new recline.View.SlickGrid({
+                model: dataset
+            })
+        },
+        {
+            id: 'graph',
+            label: 'Graph',
+            view: new recline.View.Graph({
+                model: dataset
+            })
+        },
+        {
+            id: 'map',
+            label: 'Map',
+            view: new recline.View.Map({
+                model: dataset
+            })
+        },
+        {
+            id: 'transform',
+            label: 'Transform',
+            view: new recline.View.Transform({
+                model: dataset
+            })
+        }
+    ];
+
+    window.dataExplorer = new recline.View.MultiView({
+        model: dataset,
+        el: $el,
+        state: state,
+        views: views
+    });
+}
