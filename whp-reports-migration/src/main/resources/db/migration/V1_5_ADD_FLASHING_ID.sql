bc4503673ceb8a861715b6c5efef1dc16fddb935
Balaji Narain <balajin@thoughtworks.com>
2012-11-27 16:28:06 +0530

    BalajiN | #561 | Fixing flashing_call_id migrations in environments with existing data

--- whp-reports-migration/src/main/resources/db/migration/V1_5_ADD_FLASHING_ID.sql
+++ whp-reports-migration/src/main/resources/db/migration/V1_5_ADD_FLASHING_ID.sql
@@ -1,2 +1,7 @@
-ALTER TABLE whp_reports.flashing_log add column flashing_call_id  varchar(32) UNIQUE  NOT NULL;
-ALTER TABLE whp_reports.adherence_call_log add column flashing_call_id  varchar(32) UNIQUE  NOT NULL;
+ALTER TABLE whp_reports.flashing_log add column flashing_call_id  varchar(32);
+UPDATE whp_reports.flashing_log set flashing_call_id='0' WHERE flashing_call_id is null;
+ALTER TABLE whp_reports.flashing_log ALTER COLUMN flashing_call_id set NOT NULL;
+
+ALTER TABLE whp_reports.adherence_call_log add column flashing_call_id  varchar(32);
+UPDATE whp_reports.adherence_call_log set flashing_call_id='0' WHERE flashing_call_id is null;
+ALTER TABLE whp_reports.adherence_call_log ALTER COLUMN flashing_call_id set NOT NULL;
