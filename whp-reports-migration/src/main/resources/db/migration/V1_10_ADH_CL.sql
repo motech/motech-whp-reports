812964fe8dfbc0c92d45e859fd925706db3687a6
Vishal Naik <vishalnaik@thoughtworks.com>
2012-12-28 11:17:34 +0530

    Vishal #0 Made adherence flashing call id column as  nullable to take care of inbound calls

--- whp-reports-migration/src/main/resources/db/migration/V1_10_ADH_CL.sql
+++ whp-reports-migration/src/main/resources/db/migration/V1_10_ADH_CL.sql
@@ -1,3 +1,4 @@
 ALTER TABLE whp_reports.adherence_call_log add column attempt_time timestamp;
 ALTER TABLE whp_reports.adherence_call_log add column call_answered varchar(30);
 ALTER TABLE whp_reports.adherence_call_log add column disconnection_type varchar(30);
+ALTER TABLE whp_reports.adherence_call_log ALTER COLUMN flashing_call_id drop NOT NULL;
