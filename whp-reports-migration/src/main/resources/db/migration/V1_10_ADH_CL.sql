25d1f9b3ddedca0c5127b8e74ab45a1e31de88b6
Vishal Naik <vishalnaik@thoughtworks.com>
2013-01-28 08:58:37 +0530

    Vishal #0 update value for new column adherence_call_log.attempt_time as start_date_time

--- whp-reports-migration/src/main/resources/db/migration/V1_10_ADH_CL.sql
+++ whp-reports-migration/src/main/resources/db/migration/V1_10_ADH_CL.sql
@@ -2,3 +2,4 @@ ALTER TABLE whp_reports.adherence_call_log add column attempt_time timestamp;
 ALTER TABLE whp_reports.adherence_call_log add column call_answered varchar(30);
 ALTER TABLE whp_reports.adherence_call_log add column disconnection_type varchar(30);
 ALTER TABLE whp_reports.adherence_call_log ALTER COLUMN flashing_call_id drop NOT NULL;
+update whp_reports.adherence_call_log set attempt_time = start_date_time where attempt_time is null;
