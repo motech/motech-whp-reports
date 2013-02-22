9d6d1b2026ca26b9fcbb64b29e9acd941df6e3e7
Vishal Naik <vishalnaik@thoughtworks.com>
2013-02-22 19:14:08 +0530

    Vishal, Nishi #647 Fixing column lengths

--- whp-reports-migration/src/main/resources/db/migration/V1_16_ADH_RECORD.sql
+++ whp-reports-migration/src/main/resources/db/migration/V1_16_ADH_RECORD.sql
@@ -1,7 +1,7 @@
 CREATE TABLE whp_reports.adherence_record
  (
     patient_id varchar(32),
-    therapy_id varchar(30),
+    therapy_id varchar(32),
     tb_id varchar(30),
     provider_id varchar(30),
     pill_date date,
