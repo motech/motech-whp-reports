298a82dbac5f9dd394a069582b7b91a0d8926976
Abhishek <asharma@thoughtworks.com>
2013-03-19 11:25:55 +0530

    Abhi #0 Made UserGivenDetails as serializable

--- whp-reports-contract/src/main/java/org/motechproject/whp/reports/contract/UserGivenPatientDetails.java
+++ whp-reports-contract/src/main/java/org/motechproject/whp/reports/contract/UserGivenPatientDetails.java
@@ -2,8 +2,10 @@ package org.motechproject.whp.reports.contract;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class UserGivenPatientDetails {
+public class UserGivenPatientDetails implements Serializable {
     private String patientId;
     private String patientName;
     private Integer patientAge;
