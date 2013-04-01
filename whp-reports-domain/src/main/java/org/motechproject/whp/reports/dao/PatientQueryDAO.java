ceb09d58bbfceb64dae2f7011a52c879c18ca224
Vishal Naik <vishalnaik@thoughtworks.com>
2013-04-01 14:56:07 +0530

    Vishal, Nishi #749 filtering patients in do not call list

--- whp-reports-domain/src/main/java/org/motechproject/whp/reports/dao/PatientQueryDAO.java
+++ whp-reports-domain/src/main/java/org/motechproject/whp/reports/dao/PatientQueryDAO.java
@@ -25,7 +25,12 @@ public class PatientQueryDAO {
     private static final String ACTIVE_PATIENTS_WITH_MISSING_ADHERENCE = "select patient_id, phone_no as mobile_number, " +
             "adherence_missing as missing_weeks from whp_reports.patient inner join whp_reports.patient_alert " +
             "on patient_alert_fk = alert_pk " +
-            "where adherence_missing > 0 and phone_no is not null and is_active = 'Y' order by patient_pk limit ? offset ?";
+            "where adherence_missing > 0 and phone_no is not null and is_active = 'Y' " +
+            "and not exists (select 1 from whp_reports.do_not_call dnc " +
+            "where patient.patient_id = dnc.entity_id and dnc.entity = 'Patient' " +
+            "and dnc.mobile_number = patient.phone_no) " +
+            "order by patient_pk limit ? offset ?";
+
     public List<PatientAdherenceSummary> findActivePatientsWithMissingAdherenceAndAMobileNumber(int skip, int limit) {
         return jdbcTemplate.query(ACTIVE_PATIENTS_WITH_MISSING_ADHERENCE,
                 new BeanPropertyRowMapper(PatientAdherenceSummary.class), limit, skip);
--- whp-reports-domain/src/test/java/org/motechproject/whp/reports/dao/PatientQueryDAOIT.java
+++ whp-reports-domain/src/test/java/org/motechproject/whp/reports/dao/PatientQueryDAOIT.java
@@ -2,6 +2,8 @@ package org.motechproject.whp.reports.dao;
 
 import org.junit.After;
 import org.junit.Test;
+import org.motechproject.donotcall.domain.DoNotCallEntry;
+import org.motechproject.donotcall.repository.DoNotCallEntryRepository;
 import org.motechproject.whp.reports.IntegrationTest;
 import org.motechproject.whp.reports.builder.PatientBuilder;
 import org.motechproject.whp.reports.contract.query.PatientAdherenceSummary;
@@ -19,6 +21,8 @@ public class PatientQueryDAOIT extends IntegrationTest {
 
     @Autowired
     PatientRepository patientRepository;
+    @Autowired
+    DoNotCallEntryRepository doNotCallEntryRepository;
     @Autowired
     PatientQueryDAO patientQueryDAO;
 
@@ -71,9 +75,54 @@ public class PatientQueryDAOIT extends IntegrationTest {
         assertThat(patientAdherenceSummaries.get(1).getPatientId(), is(expectedPatient2.getPatientId()));
     }
 
+    @Test
+    public void shouldNotFetchPatientsInDoNotCallList() {
+
+        Patient expectedPatient1 = new PatientBuilder()
+                .withDefaults()
+                .withPatientId("patient1")
+                .withAdherenceMissingWeeks(2)
+                .withMobileNumber("1234567890")
+                .build();
+        Patient patientInDoNotCallList = new PatientBuilder()
+                .withDefaults()
+                .withPatientId("patient2")
+                .withAdherenceMissingWeeks(2)
+                .withMobileNumber("1234567890")
+                .build();
+        Patient patientWithDifferentPhoneNumber = new PatientBuilder()
+                .withDefaults()
+                .withPatientId("patient3")
+                .withAdherenceMissingWeeks(2)
+                .withMobileNumber("0987654321")
+                .build();
+        patientRepository.save(asList(expectedPatient1, patientInDoNotCallList, patientWithDifferentPhoneNumber));
+
+        createDoNotCallEntry("Patient", "patient2", "1234567890");
+        createDoNotCallEntry("Patient", "patient3", "1234567890");
+
+        List<PatientAdherenceSummary> patientAdherenceSummaries = patientQueryDAO.findActivePatientsWithMissingAdherenceAndAMobileNumber(0, 5);
+
+        assertThat(patientAdherenceSummaries.size(), is(2));
+        assertThat(patientAdherenceSummaries.get(0).getMobileNumber(), is(expectedPatient1.getPhoneNumber()));
+        assertThat(patientAdherenceSummaries.get(0).getPatientId(), is(expectedPatient1.getPatientId()));assertThat(patientAdherenceSummaries.get(0).getMobileNumber(), is(expectedPatient1.getPhoneNumber()));
+        assertThat(patientAdherenceSummaries.get(1).getMobileNumber(), is(patientWithDifferentPhoneNumber.getPhoneNumber()));
+        assertThat(patientAdherenceSummaries.get(1).getPatientId(), is(patientWithDifferentPhoneNumber.getPatientId()));
+
+    }
+
+    private void createDoNotCallEntry(String entity, String entityId, String mobileNumber) {
+        DoNotCallEntry doNotCallPatient1 = new DoNotCallEntry();
+        doNotCallPatient1.setEntity(entity);
+        doNotCallPatient1.setEntityId(entityId);
+        doNotCallPatient1.setMobileNumber(mobileNumber);
+        doNotCallEntryRepository.save(doNotCallPatient1);
+    }
+
     @After
     public void tearDown() {
         patientRepository.deleteAll();
+        doNotCallEntryRepository.deleteAll();
     }
 }
 
