6ad2b4b0acc54fd269025127d44463baf105747b
nningego <nishitha.ningegowda@gmail.com>
2013-04-24 15:37:41 +0530

    Nishi adding a few inserts to dbUnit

--- whp-reports-domain/src/test/java/org/motechproject/whp/reports/query/CustomDataSetLoader.java
+++ whp-reports-domain/src/test/java/org/motechproject/whp/reports/query/CustomDataSetLoader.java
@@ -2,13 +2,22 @@ package org.motechproject.whp.reports.query;
 
 import com.github.springtestdbunit.dataset.AbstractDataSetLoader;
 import org.dbunit.dataset.IDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.dataset.ReplacementDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.joda.time.LocalDate;
+import org.motechproject.whp.reports.date.WHPDate;
 import org.springframework.core.io.Resource;
+
+import java.sql.Date;
 
 public class CustomDataSetLoader extends AbstractDataSetLoader {
     protected IDataSet createDataSet(Resource resource) throws Exception {
-        FlatXmlDataSet dataSet = new FlatXmlDataSetBuilder().build(resource.getURL());
-        return dataSet;
+
+        Date currentDate = WHPDate.toSqlDate(new LocalDate());
+        ReplacementDataSet replacementDataSet = new ReplacementDataSet(new FlatXmlDataSetBuilder().build(resource.getURL()));
+
+        replacementDataSet.addReplacementObject("[CURRENT_DATE]", currentDate);
+        return replacementDataSet;
+
     }
 }
