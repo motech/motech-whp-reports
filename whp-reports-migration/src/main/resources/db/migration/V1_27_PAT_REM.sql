bf03814a14a03d2c017fe151a545aa1dd5840677
MohitGarg2905 <mohit.garg2905@gmail.com>
2014-07-09 19:50:39 +0530

    Mohit|Corrected patient_reminder_call_log_table create script

--- whp-reports-migration/src/main/resources/db/migration/V1_27_PAT_REM.sql
+++ whp-reports-migration/src/main/resources/db/migration/V1_27_PAT_REM.sql
@@ -1,15 +1,23 @@
-CREATE TABLE whp_reports.patient_reminder_call_log (
-    call_log_id serial NOT NULL,
-    call_id varchar(50) UNIQUE,
-    request_id varchar(255),
-    patient_id varchar(20),
-    start_date_time timestamp,
-    call_answered varchar(20),
-    end_date_time timestamp,
-    attempt_date_time timestamp,
-    attempt numeric,
-    disconnection_type varchar(30),
-    reminder_type varchar(100),
-    mobile_number varchar(15),
-    PRIMARY KEY( call_log_id )
+CREATE TABLE whp_reports.patient_reminder_call_log
+(
+  call_log_id serial NOT NULL,
+  call_id character varying(32),
+  request_id character varying(32),
+  patient_id character varying(20),
+  start_date_time timestamp without time zone,
+  call_answered character varying(20),
+  end_date_time timestamp without time zone,
+  attempt_date_time timestamp without time zone,
+  attempt numeric,
+  disconnection_type character varying(30),
+  reminder_type character varying(100),
+  status character varying(30),
+  mobile_number character varying(15),
+  CONSTRAINT patient_reminder_call_log_pkey PRIMARY KEY (call_log_id),
+  CONSTRAINT patient_reminder_call_log_call_id_key UNIQUE (call_id)
+)
+WITH (
+  OIDS=FALSE
 );
+ALTER TABLE whp_reports.patient_reminder_call_log
+  OWNER TO postgres;
